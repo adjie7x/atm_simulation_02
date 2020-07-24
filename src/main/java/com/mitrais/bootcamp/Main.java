aa9ae71164e91c3d3d64f76b5301ba01ffd03bb6
aji_mulyadi <adjie.mulyadi7x@gmail.com>
2020-07-24 09:31:35 +0700

    add gitignore and initiate maven project

---
+++ src/main/java/com/mitrais/bootcamp/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004‐2020 All Rights Reserved.
+ */
+package com.mitrais.bootcamp;
+
+/**
+ * @author Aji Atin Mulyadi
+ * @version $Id: Main.java, v 0.1 2020‐07‐24 9:31 Aji Atin Mulyadi Exp $$
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
